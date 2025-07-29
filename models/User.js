451fa1728fbae05ed48ece7bf8cad826cc78d767
Swaraj Khair <swarajkhair@Swarajs-MacBook-Air.local>
2025-07-29 21:16:32 +0530

    feat: create user in models

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    uniquej: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: Number,
+    enum: [0, 1, 2],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = mongoose.model("user", UserSchema);
+export default User;
