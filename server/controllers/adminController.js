70b25b11932be948cdeb46cf88336e5e78ee2e0c
Swaraj Khair <swarajkhair@Swarajs-MacBook-Air.local>
2025-08-11 14:31:57 +0530

    backend fix

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -39,16 +39,7 @@ export const addDept = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
   try {
-    const {
-      username,
-      name,
-      password,
-      cons_budget,
-      cons_expenditure,
-      equip_budget,
-      equip_expenditure,
-      year,
-    } = req.body;
+    const { username, name, password, budget, expenditure, in_process,year } = req.body;
     let user = await User.findOne({ username });
     if (user) {
       return res.status(400).json({ error: "Username already exists!" });
@@ -56,22 +47,24 @@ export const addDept = async (req, res) => {
     let entry = await Consumable.create({
       username,
       department: name,
-       budget:cons_budget,
-      expdenditure:cons_expenditure,
+      budget,
+      expenditure,
+      in_process,
       year,
       indents_process: [],
       direct_purchase: [],
-      indent_pay_done: [],
+      // indent_pay_done: [],
     });
     let entry2 = await Equipment.create({
       username,
       department: name,
-      budget:equip_budget,
-      expenditure:equip_expenditure,
+      budget,
+      expenditure,
+      in_process,
       year,
       indents_process: [],
       direct_purchase: [],
-      indent_pay_done: [],
+      // indent_pay_done: [],
     });
     const salt = await bcrypt.genSalt(10);
     let secPass = await bcrypt.hash(password, salt);
@@ -88,3 +81,13 @@ export const addDept = async (req, res) => {
     res.status(500).send("Some error occured!");
   }
 };
+
+
+//{     "username":"mems2",
+// "password":"password",
+// "name":"MM 310",
+// "budget":9000000,
+// "expenditure":3444440,
+// "in_process":0,
+// "year":2025
+// }
--- server/routes/budgetRoutes.js
+++ server/routes/budgetRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { fetchSummary, addEntry, fetchTable } from "../controllers/budgetController.js";
+import { fetchSummary, addEntry, fetchTable , deleteAll} from "../controllers/budgetController.js";
 
 const router = express.Router();
 
@@ -18,5 +18,11 @@ const router = express.Router();
     "/fetchsummary",
     fetchSummary
   )
+
+  router.post(
+    "/deleteAll",
+    deleteAll
+  )
+
 
 export default router;
