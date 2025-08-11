ce8e928e629e4f701647405294931c8528d9c0bc
Swaraj Khair <swarajkhair@Swarajs-MacBook-Air.local>
2025-08-11 15:22:10 +0530

    remove user and update user added in the view all page

--- client/src/context/year/YearState.js
+++ client/src/context/year/YearState.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import YearContext from "./YearContext";
 
 const YearState = (props) => {
   const curDate = new Date();
-  const curYear = curDate.getFullYear();
   const curMonth = curDate.getMonth();
-  const [year, setYear] = useState(curMonth < 3 ? curYear - 1 : curYear);
+  const curYear = curDate.getFullYear() - (curMonth < 3);
+  const [year, setYear] = useState(() => {
+    const storedData = localStorage.getItem("year");
+    return storedData ? storedData : curYear;
+  });
+  useEffect(() => {
+    localStorage.setItem("year", year);
+  }, [year]);
 
   return (
     <YearContext.Provider value={{ year, setYear }}>
